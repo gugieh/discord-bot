c5615b2b5d17a84d71fae7a46e3bb7cc729ae61e
Adam Batko <gugieh320@gmail.com>
2024-03-25 18:24:49 +0100

    simple music bot

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const { Client, IntentsBitField } = require('discord.js')
-const { token } = require('./config.json');
+const { joinVoiceChannel } = require('@discordjs/voice');
+const { useMainPlayer } = require('discord-player');
+const { Player } = require('discord-player');
 
 const client = new Client({
     intents: [
@@ -7,8 +10,11 @@ const client = new Client({
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildVoiceStates
     ]
 });
+
+const player = new Player(client);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
@@ -24,4 +30,42 @@ client.on('messageCreate', (message) => {
     }
 })
 
-client.login(token)
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) {
+        return;
+    }
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+    if (commandName === 'play') {
+
+        const player = useMainPlayer();
+        player.extractors.loadDefault()
+        const channel = interaction.member.voice.channel;
+        if (!channel) return interaction.reply('You are not connected to a voice channel!'); // make sure we have a voice channel
+        const query = interaction.options.getString('song', true); // we need input/query to play
+
+        // let's defer the interaction as things can take time to process
+        await interaction.deferReply();
+
+        try {
+            const { track } = await player.play(channel, query, {
+                nodeOptions: {
+                    // nodeOptions are the options for guild node (aka your queue in simple word)
+                    metadata: interaction // we can access this metadata object using queue.metadata later on
+                }
+            });
+
+            return interaction.followUp(`**${track.title}** enqueued!`);
+        } catch (e) {
+            // let's return error if something failed
+            return interaction.followUp(`Something went wrong: ${e}`);
+        }
+
+    }
+})
+
+client.login(process.env.TOKEN)
---
+++ register-commands.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const { REST, Routes, Application, ApplicationCommand, ApplicationCommandOptionType } = require('discord.js');
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!'
+    },
+    {
+        name: 'play',
+        description: 'Plays a song from YouTube',
+        options: [
+            {
+                name: 'song',
+                type: ApplicationCommandOptionType.String,
+                description: 'The song you want to play',
+                required: true
+            }
+        ],
+        member: {
+            voice: {
+                channel: {
+                    required: true,
+                    type: ApplicationCommandOptionType.Channel
+                }
+            }
+        }
+    }
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands },
+        );
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error('There was an error while registering the commands:', error);
+    }
+})();

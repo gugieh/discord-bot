ddfd6982c671c069c0360bfde7da6d66020e9228
Adam Batko <gugieh320@gmail.com>
2024-03-26 13:59:27 +0100

    test website

--- index.js
+++ index.js
@@ -80,3 +80,26 @@ client.on('interactionCreate', async (interaction) => {
 })
 
 client.login(process.env.TOKEN)
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, 'utf8', (err, content) => {
+        if (err) {
+            res.writeHead(500);
+            res.end('Internal Server Error');
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Serwer działa na porcie ${PORT}`);
+});
